8e3de94b7392d4b1b15d7e118367185ccd012fea
Jesse <jessemerge2@gmail.com>
2024-10-23 01:18:20 -0700

    Redesign event cards

--- src/components/Module/Events.vue
+++ src/components/Module/Events.vue
@@ -2,7 +2,33 @@
 	<module-events>
 		<SectionTitle text="Upcoming Events" />
 		<div class="events-container" id="section-content">
-			<p v-if="events.length > 0">Come join the community!</p>
+			<div class="event-card">
+				<picture class="flyer">
+					<img
+						src="/public/images/event/punk-park.jpg"
+						alt="punk-park event flyer"
+					/>
+				</picture>
+
+				<div>
+					<header-content>
+						<h2 class="step-1">PUNK IN THE PARK</h2>
+						<p class="details">
+							A day of music and craft beer tasting, featuring 8 awesome
+							bands spanning the punk rock genre.
+						</p>
+					</header-content>
+					<text-content>
+						<p>Saturday, November 16th 2024</p>
+						<p>Thrive Park @ Snapdragon Stadium</p>
+						<p>San Diego, CA</p>
+					</text-content>
+
+					<a href="https://www.punkinthepark.com/" class="detail-link">
+						Check It Out
+					</a>
+				</div>
+				<!-- <p v-if="events.length > 0">Come join the community!</p>
 			<p v-else>Currently we are not attending any events.</p>
 			<div v-if="events.length > 0" class="calendar">
 				<Event v-for="event in events" :key="event.id" :event="event" />
@@ -12,9 +38,10 @@
 				<p>No Upcoming Events.</p>
 			</div>
 
-			<!-- <div class="calendar no-events">
+			<div class="calendar no-events">
 				<p>No Upcoming Events.</p>
 			</div> -->
+			</div>
 		</div>
 	</module-events>
 </template>
@@ -28,49 +55,101 @@
 
 <style lang="scss" scoped>
 	module-events {
-		display: flex;
-		flex-direction: column;
+		display: grid;
+		grid-template-rows: auto 1fr;
+
 		gap: 30px;
 		width: 100%;
 		position: relative;
 
-		.brand {
-			color: var(--brand-two-color);
-			border-bottom: 1px solid var(--brand-two-color);
+		.events-container {
+			width: 100%;
+			display: block;
+			height: 100%;
+			background-color: rgb(76, 47, 79);
+			border-radius: 15px;
+			overflow: hidden;
+			padding: 10px;
+
+			.event-card {
+				border-radius: 10px;
+				overflow: hidden;
+
+				display: flex;
+				flex-direction: column;
+				gap: 10px;
+				padding-bottom: 10px;
+				align-items: center;
+
+				div {
+					display: flex;
+					flex-direction: column;
+					width: 100%;
+					justify-content: center;
+					align-items: center;
+					gap: 25px;
+
+					.detail-link {
+						background-color: rgb(0, 0, 0);
+						color: white;
+						padding: 10px 25px;
+						border-radius: 8px;
+						text-decoration: none;
+						font-weight: 600;
+						transition: background-color 0.3s ease-in-out;
+
+						&:hover {
+							background-color: rgba(0, 0, 0, 0.516);
+						}
+					}
+				}
+
+				.flyer {
+					max-width: 450px;
+					overflow: hidden;
+					border-radius: 10px;
+				}
+
+				header-content {
+					align-items: center;
+					display: flex;
+					flex-direction: column;
+					width: 100%;
+					gap: 10px;
+
+					h2 {
+						text-align: center;
+						font-weight: 500;
+						max-width: fit-content;
+						border-bottom: 2px solid white;
+					}
+
+					p.details {
+						max-width: 85%;
+						text-align: center;
+						font-size: 90%;
+					}
+				}
+
+				text-content {
+					font-weight: 400;
+					display: flex;
+					flex-direction: column;
+					max-width: 85%;
+					text-align: center;
+				}
+			}
 		}
 
-		.calendar {
-			width: 100%;
-			padding: 20px 12px;
-			display: grid;
-			column-gap: 30px;
-			row-gap: 60px;
-			grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-			place-items: center;
-			min-height: 350px;
-			background: rgba(255, 255, 255, 0.12);
-
-			backdrop-filter: blur(5px);
-
-			// border-radius: 10px;
-			// border: 1px solid rgba(255, 255, 255, 0.18);
-			// overflow: hidden;
-
-			@media (min-width: 620px) {
-				grid-template-columns: repeat(auto-fill, minmax(275px, 1fr));
+		@media (min-width: 600px) {
+			.events-container {
+				.event-card {
+					flex-direction: row;
+					padding: 0%;
+					gap: 15px;
+					// padding: 10px;
+				}
 			}
-
-			&.no-events {
-				grid-template-columns: 1fr;
-			}
-		}
-
-		div.events-container {
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			align-items: flex-start;
-			gap: 20px;
 		}
 	}
 </style>
--- src/services/EventsService.js
+++ src/services/EventsService.js
@@ -2,20 +2,20 @@ import { defineStore } from 'pinia';
 
 export const useEventService = defineStore('e', () => {
 	const events = ref([
-		{
-			id: 1,
-			title: 'Alive Together - Uniting to Preventing Suicide',
-			img: '/images/event/alive.png',
-			dateDay: '29',
-			month: 'Sept.',
-			detail: 'Join us for a 5K fun walk/run to raise awareness, build community, and benefit the Didi Hirsch Suicide Prevention Center.',
-			fullDate: '',
-			timeSpan: ['8:00 am', 'September 29, 2024'],
-			location: ['UCLA Campus', 'Wilson Plaza'],
-			link: 'https://didihirsch.akaraisin.com/ui/at24',
-			linkText: 'https://didihirsch.akaraisin.com/ui/at24',
-			flyer: '/images/event/alive-flyer.JPG',
-		},
+		// {
+		// 	id: 1,
+		// 	title: 'Alive Together - Uniting to Preventing Suicide',
+		// 	img: '/images/event/alive.png',
+		// 	dateDay: '29',
+		// 	month: 'Sept.',
+		// 	detail: 'Join us for a 5K fun walk/run to raise awareness, build community, and benefit the Didi Hirsch Suicide Prevention Center.',
+		// 	fullDate: '',
+		// 	timeSpan: ['8:00 am', 'September 29, 2024'],
+		// 	location: ['UCLA Campus', 'Wilson Plaza'],
+		// 	link: 'https://didihirsch.akaraisin.com/ui/at24',
+		// 	linkText: 'https://didihirsch.akaraisin.com/ui/at24',
+		// 	flyer: '/images/event/alive-flyer.JPG',
+		// },
 	]);
 	return { events };
 });
