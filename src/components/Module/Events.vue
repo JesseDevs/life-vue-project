caeac3af8c2d4e565738adadea9121dc6f118a31
Jesse <jessemerge2@gmail.com>
2024-11-25 19:55:33 -0800

    Add new kroq event

--- src/components/Module/Events.vue
+++ src/components/Module/Events.vue
@@ -5,6 +5,53 @@
 			<div class="event-card">
 				<picture class="flyer">
 					<img
+						src="/public/images/event/kroqaac-flyer.jpg"
+						alt="kroqaac event flyer"
+					/>
+				</picture>
+
+				<div>
+					<div>
+						<header-content>
+							<h2 class="step-1">KROQ Almost Acoustic Christmas</h2>
+							<p class="details">
+								The 33rd Annual KROQ Almost Acoustic Christmas returns to
+								<a href="https://www.instagram.com/kroq/?hl=en">
+									@kiaforum</a
+								>. Presented by
+								<a href="https://www.instagram.com/thekiaforum/?hl=en">
+									@kroq
+								</a>
+								<a href="https://www.instagram.com/audacy/?hl=en">
+									@audacy
+								</a>
+								+
+								<a href="https://www.instagram.com/ilovegain/?hl=en">
+									@ilovegain
+								</a>
+								#KROQXMAS
+							</p>
+						</header-content>
+						<text-content>
+							<p>Saturday, December 14th 2024</p>
+							<p>Kia Forum</p>
+							<p>Los Angeles, CA</p>
+						</text-content>
+						<a
+							href="https://www.audacy.com/kroq/latest/33rd-annual-kroq-almost-acoustic-christmas"
+							class="detail-link"
+						>
+							Check It Out
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="event-card">
+				<past-event>
+					<h2>PAST EVENT</h2>
+				</past-event>
+				<picture class="flyer">
+					<img
 						src="/public/images/event/punk-park.jpg"
 						alt="punk-park event flyer"
 					/>
@@ -28,19 +75,6 @@
 						Check It Out
 					</a>
 				</div>
-				<!-- <p v-if="events.length > 0">Come join the community!</p>
-			<p v-else>Currently we are not attending any events.</p>
-			<div v-if="events.length > 0" class="calendar">
-				<Event v-for="event in events" :key="event.id" :event="event" />
-			</div>
-
-			<div v-else class="calendar no-events">
-				<p>No Upcoming Events.</p>
-			</div>
-
-			<div class="calendar no-events">
-				<p>No Upcoming Events.</p>
-			</div> -->
 			</div>
 		</div>
 	</module-events>
@@ -64,7 +98,9 @@
 
 		.events-container {
 			width: 100%;
-			display: block;
+			display: flex;
+			flex-direction: column;
+			gap: 30px;
 			height: 100%;
 			background-color: rgb(76, 47, 79);
 			border-radius: 15px;
@@ -75,11 +111,20 @@
 				border-radius: 10px;
 				overflow: hidden;
 
+				position: relative;
+
 				display: flex;
 				flex-direction: column;
 				gap: 10px;
 				padding-bottom: 10px;
 				align-items: center;
+
+				a:not(.detail-link) {
+					font-size: 90%;
+					&:hover {
+						color: var(--brand-color);
+					}
+				}
 
 				div {
 					display: flex;
@@ -106,8 +151,21 @@
 
 				.flyer {
 					max-width: 450px;
+
 					overflow: hidden;
 					border-radius: 10px;
+
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					height: 500px;
+
+					img {
+						max-height: 100%; /* Limit image height to container height */
+						width: auto;
+						object-fit: contain; /* Maintain aspect ratio */
+						border-radius: 10px;
+					}
 				}
 
 				header-content {
@@ -137,6 +195,23 @@
 					flex-direction: column;
 					max-width: 85%;
 					text-align: center;
+				}
+
+				past-event {
+					background-color: rgba(0, 0, 0, 0.55);
+					border-radius: 10px;
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					pointer-events: none;
+					display: grid;
+					place-items: center;
+					h2 {
+						font-weight: 800;
+						color: white;
+						font-size: var(--step-4);
+						letter-spacing: 1px;
+					}
 				}
 			}
 		}
